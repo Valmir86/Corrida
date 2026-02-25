f2972df6c87821c10a3c2b539213616b16f174ae
Valmir Pereira <110244802+Valmir86@users.noreply.github.com>
2026-02-24 21:09:15 -0300

    Update database connection parameters

--- conexao.php
+++ conexao.php
@@ -1,12 +1,12 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "fastdrive";
+$host = "sql303.infinityfree.com";
+$user = "if0_41240198";
+$pass = "De86107725";
+$db   = "if0_41240198_fastdrive";
 
 $conn = new mysqli($host, $user, $pass, $db);
 
-if($conn->connect_error){
+if ($conn->connect_error) {
     die("Erro na conexão: " . $conn->connect_error);
 }
 ?>
